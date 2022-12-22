bb3fc532a66fe5b29280208b2fb22380ef4d90e0
intertext.no <orstavik77@gmail.com>
2022-12-22 09:26:29 +0100

    demo

---
+++ src/CmsReactions.js
@@ -0,0 +1,19 @@
+class FetchError extends Error{
+  constructor(response) {
+    super(response.statusText);
+    this.response = response;
+  }
+}
+
+async function fetch2xx(...args){
+  const resp = await fetch(...args);
+  if(resp.status >= 200 && resp.status < 300)
+    return resp;
+  throw new FetchError(resp);
+}
+
+export async function fetchGet(e, prefix) {
+  debugger
+  return await (await fetch2xx("/" + prefix.toUpperCase())).text();
+}
+
